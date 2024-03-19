6bd1cead0ded894edc756d2eb2082f51cec13cde
Tracy Lin <bambo429@gmail.com>
2024-03-19 17:21:35 -0400

    padding for conv2d

--- encoder.py
+++ encoder.py
@@ -15,9 +15,11 @@ from collections import OrderedDict
 class Encoder(torch.nn.Module):
     
     def __init__(self):
+        super(Encoder, self).__init__()
         
         self.lidar = LiDAR()
         self.map = Map()
+        
         self.bottleneck_block1 = nn.Sequential(
             BottleNeck(in_channel=96, out_channel=128, hidden_channel=32, stride=2),
             BottleNeck(in_channel=128, out_channel=128, hidden_channel=32, stride=1)
@@ -57,7 +59,7 @@ class Encoder(torch.nn.Module):
             )
         
         fpn_batchnorm = torch.nn.BatchNorm2d(32)
-        self.fpn = torchvision.ops.FeaturePyramidNetwork([128, 192, 256, 384, 512], 32, fpn_batchnorm)
+        self.fpn = torchvision.ops.FeaturePyramidNetwork([128, 192, 256, 384, 512], 32)
         
         self.pooling = SpatialPyramidPooling(in_channels=32, concat_mode=1)
         
@@ -66,8 +68,11 @@ class Encoder(torch.nn.Module):
         x_lidar = self.lidar(x_lidar)
         x_map = self.map(x_map)
         
+        print('lidar', x_lidar.size())
+        print('map', x_map.size())
         # concat to [96,H,W]
-        x = torch.cat((x_lidar, x_map), 0)
+        x = torch.cat((x_lidar, x_map), 1)
+        print('cat', x.size())
         
         x_pyramid = OrderedDict()
         
@@ -104,11 +109,12 @@ class Encoder(torch.nn.Module):
         
 class LiDAR(torch.nn.Module):    
     def __init__(self):
+        super(LiDAR, self).__init__()
         
-        self.conv1 = torch.nn.Conv2d(160, 32, 3, 1)
+        self.conv1 = torch.nn.Conv2d(160, 32, kernel_size=3, stride=1, padding=1)
         self.batchnorm = torch.nn.BatchNorm2d(32)
-        self.relu = torch.nn.ReLu()
-        self.conv2 = torch.nn.Conv2d(32, 32, 3, 1)
+        self.relu = torch.nn.ReLU()
+        self.conv2 = torch.nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1)
         
         
     def forward(self, x):
@@ -127,11 +133,12 @@ class LiDAR(torch.nn.Module):
 class Map(torch.nn.Module):
     
     def __init__(self):
+        super(Map, self).__init__()
         
-        self.conv1 = torch.nn.Conv2d(1, 64, 3, 1)
+        self.conv1 = torch.nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1)
         self.batchnorm = torch.nn.BatchNorm2d(64)
-        self.relu = torch.nn.ReLu()
-        self.conv2 = torch.nn.Conv2d(64, 64, 3, 1)
+        self.relu = torch.nn.ReLU()
+        self.conv2 = torch.nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
         
     def forward(self, x):
         
@@ -151,14 +158,15 @@ class Map(torch.nn.Module):
 class BottleNeck(torch.nn.Module):
     
     def __init__(self, in_channel, out_channel, hidden_channel, stride=1):
+        super(BottleNeck, self).__init__()
         
         self.s2 = stride
-        self.conv1 = torch.nn.Conv2d(in_channel, hidden_channel, 1, 1)
+        self.conv1 = torch.nn.Conv2d(in_channel, hidden_channel, kernel_size=1, stride=1)
         self.batchnorm_h = torch.nn.BatchNorm2d(hidden_channel)
-        self.conv2 = torch.nn.Conv2d(hidden_channel, hidden_channel, 3, self.s2)
-        self.conv3 = torch.nn.Conv2d(hidden_channel, out_channel, 1, 1)
+        self.conv2 = torch.nn.Conv2d(hidden_channel, hidden_channel, kernel_size=3, stride=self.s2, padding=1)
+        self.conv3 = torch.nn.Conv2d(hidden_channel, out_channel, kernel_size=1, stride=1)
         self.batchnorm_o = torch.nn.BatchNorm2d(out_channel)
-        self.relu = torch.nn.ReLu()
+        self.relu = torch.nn.ReLU()
         
         self.conv4 = torch.nn.Conv2d(in_channel, out_channel, 1, self.s2)
         
@@ -187,8 +195,8 @@ class BottleNeck(torch.nn.Module):
 if __name__ == '__main__':
     # some quick tests for the SPP module
     C = 10
-    x_lidar = torch.randn((160, 224, 224))
-    x_map = torch.randn(1,224,224)
+    x_lidar = torch.randn((2,160, 256, 256))
+    x_map = torch.randn(2,1,256,256)
     
     
     ENCODER = Encoder()
